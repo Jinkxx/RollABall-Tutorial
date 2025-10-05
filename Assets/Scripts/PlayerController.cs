d85c7d9a1e745b17b552afc93c315e50c1da62e7
Eir <everythinginreturninfo.com>
2025-10-05 13:23:16 -0400

    Added UI count text and you win message and logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Speed at which the player moves.
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     // Rigidbody of the player.
     private Rigidbody rb;
+    private int count;
     // Movement along X and Y axes.
     private float movementX;
     private float movementY;
@@ -20,6 +24,9 @@ public class PlayerController : MonoBehaviour
     {
         // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
 
     }
     // This function is called when a move input is detected.
@@ -31,6 +38,14 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
 
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
     // FixedUpdate is called once per fixed frame-rate frame.
     private void FixedUpdate()
@@ -47,6 +62,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
     }
 }
